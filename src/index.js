a7b4b6aa17ce71eef72352699f8b82ba3090c2cd
newtonjose <josephnewton001@gmail.com>
2021-09-02 13:34:58 -0300

    feat: reverse string

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
-const convertToF = function(ceulcius) {
+const convertToF = (ceulcius) => {
   const convertFactor = 9 / 5;
   let fahrenheit = ceulcius * convertFactor + 32;
 
   return fahrenheit;
-}
+};
 
-export { convertToF };
+const reverseString = (str) => {
+  let reversed = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str.charAt(i);
+  }
+
+  return reversed;
+};
+
+const reverseStringRecursion = (str) => {
+  // case base or terminal case
+  if (str === "") return "";
+  else return reverseStringRecursion(str.substr(1)) + str.charAt(0);
+};
+
+console.log(reverseStringRecursion("hello"));
+
+
+export { convertToF, reverseString };
--- src/tests/convertToF.test.js
+++ src/tests/convertToF.test.js
@@ -1,7 +1,13 @@
-import { convertToF } from "../index";
+import { convertToF, reverseString } from "../index";
 
-test("convertToF", function () {
+test("convertToF", () => {
   expect(convertToF(30)).toBe(86);
   expect(convertToF(0)).toBe(32);
   expect(convertToF(-30)).toBe(-22);
+});
+
+test("reverseString", () => {
+  expect(reverseString("hello")).toBe("olleh");
+  expect(reverseString("Howdy")).toBe("ydwoH");
+  expect(reverseString("Greetings from Earth")).toBe("htraE morf sgniteerG");
 });
